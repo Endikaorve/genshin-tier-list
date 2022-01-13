a46404cd56f0eb03f18061c4b328c5a49b6cda41
Endika <endikaorve@gmail.com>
2022-01-13 18:40:50 +0100

    Init: Base APP

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Genshin Impact Tier List</title>
+        <meta name='description' content='Genshin Impact Tier List'></meta>
+        <meta
+          name='keywords'
+          content='genshin, impact, genshin impact, tier list, meta, teams, top tier teams, characters, top tier characters'
+        ></meta>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
