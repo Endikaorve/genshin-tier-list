f75ba45049dae435265c47eec2a72e11d2d18607
Endika <endikaorve@gmail.com>
2022-01-16 16:58:18 +0100

    Refactor: Mejora la optención de datos

--- components/molecules/Patch/Patch.tsx
+++ components/molecules/Patch/Patch.tsx
@@ -3,13 +3,34 @@ import Tier from 'components/molecules/Tier/Tier';
 
 import { ITier, IPatch } from 'interfaces/interfaces';
 
-const Patch = ({ patch }: { patch: IPatch | undefined }) => {
-  if (!patch) return <>Parche no seleccionado</>;
+const Patch = ({
+  patches,
+  selectedPatch,
+  onPreviousPatchID,
+  onNextPatchID,
+}: {
+  patches: IPatch[];
+  selectedPatch: IPatch | undefined;
+  onPreviousPatchID: any;
+  onNextPatchID: any;
+}) => {
+  if (!selectedPatch) return <>Parche no seleccionado</>;
 
   return (
     <>
-      <PatchTitle>{`Parche ${patch.name}`}</PatchTitle>
-      {patch.tiers.map((tier: ITier, index: number) => (
+      <PatchTitle>
+        <StyledPatchSelectorArrow onClick={onPreviousPatchID}>
+          {'<'}
+        </StyledPatchSelectorArrow>
+
+        {`Parche ${selectedPatch.name}`}
+
+        <StyledPatchSelectorArrow onClick={onNextPatchID}>
+          {'>'}
+        </StyledPatchSelectorArrow>
+      </PatchTitle>
+
+      {selectedPatch.tiers.map((tier: ITier, index: number) => (
         <Tier key={index} tier={tier} index={index} />
       ))}
     </>
@@ -19,10 +40,20 @@ const Patch = ({ patch }: { patch: IPatch | undefined }) => {
 export default Patch;
 
 const PatchTitle: any = styled.div`
-  display: grid;
-  place-items: center;
-  padding: var(--space-lg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: var(--space-xl);
+  padding: var(--space-xl);
   color: white;
   font-size: 48px;
   font-weight: 700;
+`;
+
+const StyledPatchSelectorArrow = styled.span`
+  cursor: pointer;
+
+  &:hover {
+    color: rgba(192, 192, 192);
+  }
 `;
