40de13c669a8d43fec353bac401bf37237484648
Endika <endikaorve@gmail.com>
2022-01-17 01:38:22 +0100

    Feat: Aumenta el tamaño del Container

--- components/atoms/Container/Container.tsx
+++ components/atoms/Container/Container.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.main`
   display: block;
-  max-width: 992px;
+  max-width: 1200px;
   margin: 0 auto;
   padding: var(--space-md);
 `;
